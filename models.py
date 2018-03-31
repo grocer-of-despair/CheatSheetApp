9d61ba7c1ae9ed5958507b9270637c07e8ffc97a
Ruairi Douglas <ruairid@ruairid-macbookpro.roam.corp.google.com>
2018-03-31 11:53:24 +0100

    deleted unecessary vagrant

---
+++ models.py
@@ -0,0 +1,120 @@
+import sys
+import psycopg2
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+from passlib.apps import custom_app_context as pwd_context
+from itsdangerous import Serializer
+
+Base = declarative_base()
+
+
+# ADD YOUR USER MODEL HERE
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(32), index=True)
+    picture = Column(String)
+    email = Column(String)
+    password_hash = Column(String(64))
+
+    def hash_password(self, password):
+        self.password_hash = pwd_context.encrypt(password)
+
+    def verify_password(self, password):
+        return pwd_context.verify(password, self.password_hash)
+
+    def generate_auth_token(self, expiration=600):
+        s = Serializer(secret_key, expires_in=expiration)
+        return s.dumps({'id': self.id})
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(secret_key)
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            # Valid Token, but expired
+            return None
+        except BadSignature:
+            # Invalid Token
+            return None
+        user_id = data['id']
+        return user_id
+
+
+class Category(Base):
+    """
+        This defines the Category class
+    """
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    picture = Column(String)
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
+
+class Concept(Base):
+    __tablename__ = 'concept'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String)
+    code = Column(String)
+    picture = Column(String)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'picture': self.picture,
+            'code': self.code,
+        }
+
+
+class Links(Base):
+    __tablename__ = 'links'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    link = Column(String)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'link': self.link,
+            'id': self.id,
+        }
+
+
+# INSERT AT END OF FILE ###
+engine = create_engine(
+            'postgresql+psycopg2://grader:grader@localhost:5432/cheatsheet')
+Base.metadata.create_all(engine)
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
